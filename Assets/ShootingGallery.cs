8a216412f2c0b7e0f312812580d9c14a5e3663ab
alcoru <55741940+alcoru@users.noreply.github.com>
2020-10-23 19:07:07 +0200

    EnemiesHealth/Terrain/ShootingGallery_almost

---
+++ Assets/ShootingGallery.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShootingGallery : MonoBehaviour
+{
+    private int minScore = 1000;
+    private int actualScore;
+    private bool inside = false;
+    private Text textScore;
+    private int last_points = 0;
+    //[SerializeField] GameObject door;
+
+    public void Awake()
+    {
+        //textScore = GetComponentInChildren<Text>();
+    }
+
+    public void ScorePoitns(int points)
+    {   
+        actualScore += points / (points == last_points ? 3:1);
+        last_points = points;
+        Debug.Log(actualScore);
+
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if(collider.tag.Equals("Player"))
+        {
+            StartShootingGallery();
+        }
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        if(collider.tag.Equals("Player"))
+        {
+            EndShootingGallery();
+        }
+    }
+
+
+    private void StartShootingGallery()
+    {
+        inside = true;
+        actualScore = 0;
+    }
+
+    private void EndShootingGallery()
+    {
+        if(actualScore >= minScore)
+        {
+            OpenDoor();
+        }
+
+        inside = false;
+        actualScore = 0;
+    }
+
+    private void OpenDoor()
+    {
+        //Open da Door
+        //door.GetComponent<Animator>().SetBool("open", true);
+    }
+}
---
+++ Assets/target_script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class target_script : MonoBehaviour, DamageTaker
+{
+
+    [SerializeField] int points;
+    [SerializeField] string animation_name;
+    [SerializeField] Transform shootingGallery;
+    Animator animator;
+
+    public void Awake()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetBool(animation_name, true);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        shootingGallery.GetComponent<ShootingGallery>().ScorePoitns(points);
+    }
+}
