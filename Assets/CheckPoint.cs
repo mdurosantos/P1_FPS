2e89f97d50faa9608fa537e5de4295053d8e2de7
alcoru <55741940+alcoru@users.noreply.github.com>
2020-10-25 14:22:51 +0100

    CheckPoints

---
+++ Assets/CheckPoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    private CheckPointController checkPointController;
+    
+    private void OnTriggerEnter(Collider collider)
+    {
+
+        if(checkPointController == null)
+        {
+            checkPointController = CheckPointController.GetInstance();
+        }
+
+        checkPointController.changeCheckPoint(this);
+    }
+}
